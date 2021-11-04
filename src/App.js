57dd38d818e628967d02d9979e0ca6ab9385c092
Aidan C <74046191+kfc103@users.noreply.github.com>
2021-11-04 16:21:55 +0000

    update for App icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./styles.css";
-import AppBar from "./AppBar";
 import BasicTable from "./BasicTable";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material//Typography";
+import Slide from "@mui/material/Slide";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
 
+function HideOnScroll(props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 export default function App() {
   return (
     <div className="App">
-      <AppBar />
+      <HideOnScroll {...this.props}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6">Timezoner</Typography>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
       <BasicTable />
     </div>
   );
