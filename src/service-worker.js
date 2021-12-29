71415b308296261f79fdc33734fe61c793b4a765
Aidan C <74046191+kfc103@users.noreply.github.com>
2021-12-29 11:35:57 +0000

    test for injectManifest

--- src/service-worker.js
+++ src/service-worker.js
@@ -71,3 +71,19 @@ self.addEventListener("message", (event) => {
 });
 
 // Any other custom service worker logic can go here.
+const { injectManifest } = require("workbox-build");
+
+let workboxConfig = {
+  globDirectory: "dist",
+  globPatterns: ["*.js", "*.css", "favicon.ico", "index.html"],
+  swSrc: "src/service-worker.js",
+  swDest: "dist/service-worker.js",
+  // Custom size limit
+  maximumFileSizeToCacheInBytes: 4 * 1024 * 1024
+};
+
+injectManifest(workboxConfig).then(({ count, size }) => {
+  console.log(
+    `Generated ${workboxConfig.swDest}, which will precache ${count} files, totaling ${size} bytes.`
+  );
+});
