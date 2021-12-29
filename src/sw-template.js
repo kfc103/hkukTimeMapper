40a21bb6ebd52b0bfebb169143885627980f2a04
Aidan C <74046191+kfc103@users.noreply.github.com>
2021-12-29 14:21:59 +0000

    update sw-template.js

--- src/sw-template.js
+++ src/sw-template.js
@@ -10,6 +10,33 @@ if (typeof importScripts === "function") {
     /* injection point for manifest files.  */
     workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
+    // Set up App Shell-style routing, so that all navigation requests
+    // are fulfilled with your index.html shell. Learn more at
+    // https://developers.google.com/web/fundamentals/architecture/app-shell
+    const fileExtensionRegexp = new RegExp("/[^/?]+\\.[^/]+$");
+    workbox.precaching.registerRoute(
+      // Return false to exempt requests from being fulfilled by index.html.
+      ({ request, url }) => {
+        // If this isn't a navigation, skip.
+        if (request.mode !== "navigate") {
+          return false;
+        } // If this is a URL that starts with /_, skip.
+
+        if (url.pathname.startsWith("/_")) {
+          return false;
+        } // If this looks like a URL for a resource, because it contains // a file extension, skip.
+
+        if (url.pathname.match(fileExtensionRegexp)) {
+          return false;
+        } // Return true to signal that we want to use the handler.
+
+        return true;
+      },
+      workbox.precaching.createHandlerBoundToURL(
+        process.env.PUBLIC_URL + "/index.html"
+      )
+    );
+
     /* custom cache rules */
     workbox.routing.registerRoute(
       new workbox.routing.NavigationRoute(
